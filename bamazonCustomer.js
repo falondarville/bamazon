2d0bd3c8e6bf2d6ce6e844b15bc905fa0fa02bf2
Falon Darville <falondarville@gmail.com>
2018-04-12 15:47:06 -0700

    add variables

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,48 +2,57 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-	host: "localhost",
-	// homebrew is running my server, no port set = default port 3306
+    host: "localhost",
+    // homebrew is running my server, no port set = default port 3306
 
-	user: "root",
+    user: "root",
 
-	password: "",
-	database: "bamazon"
+    password: "",
+    database: "bamazon"
 });
-
-// test connection 
+// test connection
 connection.connect(function(error){
-	if (error) throw error;
-	console.log("connected to " + connection.threadId);
-	afterConnection();
+    if (error) throw error;
+    console.log("connected to " + connection.threadId);
+    afterConnection();
 });
 
 function start() {
-	inquirer.prompt(
-	{
-		name: "productId",
-		type: "input",
-		message: "What is the ID for the product you would like to purchase?"
-	},
-	{
-		name: "howMany",
-		type: "input",
-		message: "How many units would you like to purchase?"
-	}
-	).then(function(answer){
-		connection.query(
-			// check if there are enough items in stock
-			// if there are not, tell the customer that there is an insufficient quantity
-			// if there are, reduce the number from the stock available in the database and print the customer's total price of purchase
-		);
-	});
-}
+
+    connection.query("SELECT * FROM products", function(error, response) {
+        if (error) throw error;
+
+        inquirer.prompt([
+        {
+            name: "productId",
+            type: "input",
+            message: "What is the ID for the product you would like to purchase?"
+        },
+        {
+            name: "howMany",
+            type: "input",
+            message: "How many units would you like to purchase?"
+        }
+        ]).then(function(answer){
+
+            // gather the responses in variables
+            var product = answer.productId;
+            var quantity = answer.howMany;
+            console.log("You want to purchase an item with the product ID " + product + " in the quantity of " + quantity + ".");
+            // check if there are enough items in stock
+            // if there are not, tell the customer that there is an insufficient quantity
+            // if there are, reduce the number from the stock available in the database and print the customer's total price of purchase
+    });
+});
+};
+
+start();
 
 // prints all items (for testing)
 function afterConnection() {
-	connection.query("SELECT * FROM products", function(error, response){
-		if (error) throw error;
-		console.log(response);
-		connection.end();
-	});
+    connection.query("SELECT * FROM products", function(error, response){
+        if (error) throw error;
+        // console.log(response);
+        connection.end();
+    });
 }
