d20498e4971ef432c2fb4f29d9fb02b16a3564b1
Falon Darville <falondarville@gmail.com>
2018-04-11 19:12:58 -0700

    add inquirer portion

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -16,6 +17,27 @@ connection.connect(function(error){
 	console.log("connected to " + connection.threadId);
 	afterConnection();
 });
+
+function start() {
+	inquirer.prompt(
+	{
+		name: "productId",
+		type: "input",
+		message: "What is the ID for the product you would like to purchase?"
+	},
+	{
+		name: "howMany",
+		type: "input",
+		message: "How many units would you like to purchase?"
+	}
+	).then(function(answer){
+		connection.query(
+			// check if there are enough items in stock
+			// if there are not, tell the customer that there is an insufficient quantity
+			// if there are, reduce the number from the stock available in the database and print the customer's total price of purchase
+		);
+	});
+}
 
 // prints all items (for testing)
 function afterConnection() {
